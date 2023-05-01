94cf761c35780f6207de0f559c029c4daf7627ee
Pobitrodeb <pobitrodn@gmail.com>
2023-05-01 20:17:10 +0600

    get json data

--- CRUD/index.php
+++ CRUD/index.php
@@ -1,9 +1,22 @@
 <?php 
-   $name = array("Johne", "Kelvien", "Rajon"); 
-   //echo json_encode($name, JSON_FORCE_OBJECT); //JSON_FORCE_OBJECT is a represent JSON array index
+   // $name = array("Johne", "Kelvien", "Rajon"); 
+   // //echo json_encode($name, JSON_FORCE_OBJECT); //JSON_FORCE_OBJECT is a represent JSON array index
 
-   //JSON string convert to PHP 
-   $jsonString = '{"name": "Pobitro", "roll":"30214"}';
-   $phpArr = json_decode($jsonString);
-   echo $phpArr->name;
+   // //JSON string convert to PHP 
+   // $jsonString = '{"name": "Pobitro", "roll":"30214"}';
+   // $phpArr = json_decode($jsonString);
+   // echo $phpArr->name;
+
+   $data = file_get_contents("books.json");
+   
+   $phpData = json_decode($data, true);
+
+   foreach($phpData as $objects){
+     
+      foreach($objects as $key => $value ){
+         echo $key . " : " .$value . "<br/>";
+      }
+
+   }
+
 ?>
