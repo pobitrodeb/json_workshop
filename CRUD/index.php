e9864ead6cece9852126923d8d270d27a21c1885
Pobitrodeb <pobitrodn@gmail.com>
2023-05-02 05:42:48 +0600

    JSON data delete done

--- CRUD/index.php
+++ CRUD/index.php
@@ -1,40 +1,57 @@
-<?php 
-   // $name = array("Johne", "Kelvien", "Rajon"); 
-   // //echo json_encode($name, JSON_FORCE_OBJECT); //JSON_FORCE_OBJECT is a represent JSON array index
+<?php
+// $name = array("Johne", "Kelvien", "Rajon"); 
+// //echo json_encode($name, JSON_FORCE_OBJECT); //JSON_FORCE_OBJECT is a represent JSON array index
 
-   // //JSON string convert to PHP 
-   // $jsonString = '{"name": "Pobitro", "roll":"30214"}';
-   // $phpArr = json_decode($jsonString);
-   // echo $phpArr->name;
+// //JSON string convert to PHP 
+// $jsonString = '{"name": "Pobitro", "roll":"30214"}';
+// $phpArr = json_decode($jsonString);
+// echo $phpArr->name;
 
-   $data = file_get_contents("books.json");
-   
-   $phpData = json_decode($data, true);
+$id = 0;
 
-   // Create JSON Data  
-   $newData = array(
-      "id" => 17,
-      "title" => "Power of postive thiniking", 
-      "author" => "Jone Doe"
-   );
+$data = file_get_contents("books.json");
 
-   array_push($phpData, $newData);
-   file_put_contents("books.json", json_encode($phpData));
-   foreach($phpData as $objects){
-     
-      foreach($objects as $key => $value ){
-         echo $key . " : " .$value . "<br/>";
-      }
+$phpData = json_decode($data, true);
 
+// Create JSON Data  
+$newData = array(
+   "id" => 17,
+   "title" => "Power of postive thiniking",
+   "author" => "Jone Doe"
+);
+
+array_push($phpData, $newData);
+file_put_contents("books.json", json_encode($phpData));
+
+// Update JSON Data  
+foreach ($phpData as $key => $value) {
+   if ($value["id"] === 17) {
+      $phpData[$key]["author"] = "Pobitro Deb";
    }
+}
+file_put_contents("books.json", json_encode($phpData));
 
-        // Update JSON Data  
-        foreach($phpData as $key => $value){
-         if($value ["id"] === 17 ){
-            $phpData [$key]["author"] = ["Pobitro Deb"];
-         }
-      }
-      file_put_contents("books.json", json_encode($phpData));
+// Delete JSON Data  
 
-      
-?>
+$arryIndex = array();
+foreach ($phpData as $key => $value) {
+   if ($value["id"] == 17) {
+      $arryIndex[] = $key;
+   }
+}
+
+// remove 
+
+foreach($arryIndex as $i){
+   unset($phpData [$i]);
+}
+
+$phpData = array_values($phpData);
+file_put_contents("books.json", json_encode($phpData));
+
+foreach ($phpData as $objects) {
+
+   foreach ($objects as $key => $value) {
+      echo $key . " : " . $value . "<br/>";
+   }
+}
